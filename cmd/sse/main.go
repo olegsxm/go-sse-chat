f2536a50ad26fe7014c005899b1c25b1dc94e496
olegsxm <olegsxm@yandex.ru>
2024-10-10 01:27:05 +0700

    refactor os signal & context

--- cmd/sse/main.go
+++ cmd/sse/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"context"
-	"log/slog"
+	"errors"
+	"log"
+	"net/http"
 	"os"
 	"os/signal"
+	"syscall"
+	"time"
 
 	"github.com/olegsxm/go-sse-chat.git/internal/apps/sse"
 
@@ -12,10 +16,37 @@ import (
 )
 
 func main() {
-	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
-	defer cancel()
+	sig := make(chan os.Signal, 1)
+	signal.Notify(sig, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM, syscall.SIGQUIT)
 
-	if err := sse.Run(ctx); err != nil {
-		slog.Error("sse run error: ", err.Error())
+	serverCtx, serverStopCtx := context.WithCancel(context.Background())
+
+	server := sse.Run(serverCtx)
+
+	go func() {
+		<-sig
+
+		// Shutdown signal with grace period of 30 seconds
+		shutdownCtx, _ := context.WithTimeout(serverCtx, 30*time.Second)
+
+		go func() {
+			<-shutdownCtx.Done()
+			if shutdownCtx.Err() == context.DeadlineExceeded {
+				log.Fatal("graceful shutdown timed out.. forcing exit.")
+			}
+		}()
+
+		// Trigger graceful shutdown
+		err := server.Shutdown(shutdownCtx)
+		if err != nil {
+			log.Fatal(err)
+		}
+		serverStopCtx()
+	}()
+
+	if err := server.ListenAndServeTLS("server.crt", "server.key"); err != nil && !errors.Is(err, http.ErrServerClosed) {
+		panic(err)
 	}
+
+	<-serverCtx.Done()
 }
--- internal/apps/sse/main.go
+++ internal/apps/sse/main.go
@@ -2,15 +2,13 @@ package sse
 
 import (
 	"context"
-	"errors"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/olegsxm/go-sse-chat.git/internal/handlers"
 	"github.com/olegsxm/go-sse-chat.git/internal/repository"
 	"github.com/olegsxm/go-sse-chat.git/internal/use_cases"
 	"log/slog"
 	"net/http"
 	"time"
-
-	"github.com/olegsxm/go-sse-chat.git/pkg/handler"
 
 	"golang.org/x/net/http2/h2c"
 
@@ -19,11 +17,10 @@ import (
 	"golang.org/x/net/http2"
 )
 
-func Run(ctx context.Context) error {
+func Run(ctx context.Context) *http.Server {
 	slog.Info("Sse Chat Running")
 
 	r := repository.New()
-
 	us := use_cases.New(&r)
 
 	h2s := &http2.Server{
@@ -31,22 +28,17 @@ func Run(ctx context.Context) error {
 	}
 
 	mux := chi.NewRouter()
+	mux.Use(middleware.RequestID)
+	mux.Use(middleware.Recoverer)
+	mux.Use(middleware.StripSlashes)
 
 	handlers.New(ctx, mux, &us)
-
-	mux.Get("/", handler.HandleRoute(func(writer http.ResponseWriter, request *http.Request) error {
-		_, err := writer.Write([]byte("Hello, World!"))
-		return err
-	}))
 
 	server := &http.Server{
 		Addr:    ":443",
 		Handler: h2c.NewHandler(mux, h2s),
 	}
 
-	if err := server.ListenAndServeTLS("server.crt", "server.key"); err != nil && !errors.Is(err, http.ErrServerClosed) {
-		return err
-	}
+	return server
 
-	return nil
 }
